fc15d3d4739ae4856d138249d94f00afb32b3e8d
Zach Huang <zhuo-nanhuang2024@u.northwestern.edu>
2023-01-10 01:13:05 -0600

    Clear input field upon adding listing

--- script.js
+++ script.js
@@ -87,6 +87,7 @@ function addListing() {
         window.alert("Daily listing limit has been reached.")
     } else {
         pushHistory(document.getElementById("comment").value);
+        document.getElementById("comment").value = ""
         update();
     }
 }
